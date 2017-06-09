28d3d2f96243dd8eb76971bf9111d15b61b00a79
jsthompson49 <ljthompsons@yahoo.com>
2017-06-09 09:03:17 -0500

    Cleanup

--- Demo/src/main/java/com/amazonaws/lambda/demo/LambdaFunctionHandler.java
+++ Demo/src/main/java/com/amazonaws/lambda/demo/LambdaFunctionHandler.java
@@ -11,44 +11,28 @@ import java.util.Map;
 
 import org.json.JSONObject;
 
-public class LambdaFunctionHandler implements RequestHandler<Object, Map<String,Object>> {
+public class LambdaFunctionHandler implements RequestHandler<Map<String,Object>, Map<String,Object>> {
 
     @Override
-    public Map<String,Object> handleRequest(Object input, Context context) {
+    public Map<String,Object> handleRequest(Map<String,Object> input, Context context) {
         context.getLogger().log("Input: " + input);
 
         Map<String, Object> map = null;
         try {
             context.getLogger().log("Input type is " + input.getClass().getName());
-        	Map inputMap = (Map) input;
-            context.getLogger().log("Getting name");
-        	//String name = inputJson.getJSONObject("request").getJSONObject("intent").getJSONObject("slots").getJSONObject("name").getString("value");
-        	String name = ((Map)((Map)((Map)((Map)inputMap.get("request")).get("intent")).get("slots")).get("name")).get("value").toString();
+            String name = getMap(input, new String[] { "request", "intent", "slots", "name" }).get("value").toString();
         	
-        	JSONObject responseJson = new JSONObject();
+            JSONObject responseJson = new JSONObject();
         	addOutputSpeech(responseJson, "Hello " + name);
             JSONObject outputJson = new JSONObject();
         	outputJson.put("version", "1.0");
         	outputJson.put("sessionAttributes", new JSONObject());
         	outputJson.put("shouldEndSession", false);
         	outputJson.put("response", responseJson);
-       /* 	  
-        	    "card": {
-        	      "type": "Simple",
-        	      "title": "Horoscope",
-        	      "content": "Today will provide you a new learning opportunity.  Stick with it and the possibilities will be endless."
-        	    },
-        	    "reprompt": {
-        	      "outputSpeech": {
-        	        "type": "PlainText",
-        	        "text": "Can I help you with anything else?"
-        	      }
-        	}*/
             context.getLogger().log("Output: " + outputJson);
             
     		ObjectMapper mapper = new ObjectMapper();
 
-    		// read JSON from a file
     		map = mapper.readValue(outputJson.toString(), new TypeReference<Map<String, Object>>() {});
         }
         catch (Exception e) {
@@ -60,6 +44,18 @@ public class LambdaFunctionHandler implements RequestHandler<Object, Map<String,
         return map;
     }
     
+    static Map<String,Object> getMap(Map<String,Object> map, String[] keys) {
+		int count = (keys == null) ? 0: keys.length;
+			for(int i = 0;i < count;i++) {
+			if(map == null) {
+				break;
+			}
+			map = getMap(map, keys[i]);
+		}
+
+        return map;
+    }
+
     static Map<String,Object> getMap(Map<String,Object> map, String key) {
     	Object value = map.get(key);
     	if (value instanceof Map) {
