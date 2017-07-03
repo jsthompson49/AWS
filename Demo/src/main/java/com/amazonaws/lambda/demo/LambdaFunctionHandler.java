09effb53e54cd92b8b661afad2995a484582d08a
jsthompson49 <ljthompsons@yahoo.com>
2017-07-03 17:32:08 -0500

    Interact with IoT Edge Devince

--- Demo/src/main/java/com/amazonaws/lambda/demo/LambdaFunctionHandler.java
+++ Demo/src/main/java/com/amazonaws/lambda/demo/LambdaFunctionHandler.java
@@ -1,56 +1,83 @@
 package com.amazonaws.lambda.demo;
 
+import com.amazonaws.services.iot.client.AWSIotMessage;
+import com.amazonaws.services.iot.client.AWSIotMqttClient;
+import com.amazonaws.services.iot.client.AWSIotQos;
+import com.amazonaws.services.iot.client.AWSIotTopic;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import org.json.JSONObject;
 
 public class LambdaFunctionHandler implements RequestHandler<Map<String,Object>, Map<String,Object>> {
 
+	private static final String 	EDGE_DEVICE_CLIENT_ID = "AlexaRecognizer";
+	private static final String 	TOPIC = "demo/command";
+	private static final String		RESPONSE_TYPE_CAMERA = "camera";
+	private static final String		RESPONSE_REPLY_IMAGE = "image";
+	
     @Override
     public Map<String,Object> handleRequest(Map<String,Object> input, Context context) {
         context.getLogger().log("Input: " + input);
 
-        Map<String, Object> map = null;
+        Map<String, Object> output = null;
         try {
-            // Default response
-            JSONObject outputJson = buildResponse("This is not expected. Please try again.", true /* end session */);
-
             Map<String,Object> requestMap = getMap(input, "request");
             Object type = (requestMap == null) ? null : requestMap.get("type");
             if(type == null) {
-                outputJson = buildResponseBody("I don't know how to handle that.", true /* end session */);
+                output = buildEndResponseBody("I don't know how to handle that.");
             }
             else if(type.equals("LaunchRequest")) {
-                outputJson = buildResponseBody("Hello.  I don't recognize you yet.", false /* do not end session */);
+            	Future<JSONObject> edgeResponseFuture = sendEdgeRequest(null /* no image Id */, null /* no tag */, context);
+            	JSONObject edgeResponse = edgeResponseFuture.get(3000, TimeUnit.MILLISECONDS);
+            	JSONObject results = edgeResponse.getJSONObject("results");
+            	String tag = results.optString("tag");
+            	if((tag == null) || tag.isEmpty()) {
+            		HashMap<String, Object> sessionState = new HashMap<>();
+            		String imageId = results.getString("imageId");
+            		sessionState.put("imageId", imageId);
+            		context.getLogger().log("EdgeDevice capture image with id: " + imageId);
+                    output = buildResponseBody("Hello.  I don't recognize you yet. What is your name?", 
+                    		false /* do not end session */, sessionState);
+            	}
+            	else {
+                	output = buildEndResponseBody("Hello " + tag);
+            	}
             }
             else if(type.equals("IntentRequest")) {
             	Map<String,Object> nameMap = getMap(requestMap, new String[] { "intent", "slots", "name" });
             	Object value = (nameMap == null) ? null : nameMap.get("value");
-            	if(value == null) {
-                	outputJson = buildResponseBody("Hello.  Can you tell me your name?", false /* do not end session */);
-            	}
-            	else {
-                	outputJson = buildResponseBody("Hello " + value.toString(), true /* end session */);
+            	if(value != null) {
+                	output = buildEndResponseBody("Hello " + value.toString() + ".  I will recognize you next time.");
             	}
             }   
+            else if(type.equals("SessionEndedRequest")) {
+            	// TODO: Cleanup if still listening for IoT request
+            }
             
-    		ObjectMapper mapper = new ObjectMapper();
-    		map = mapper.readValue(outputJson.toString(), new TypeReference<Map<String, Object>>() {});
+        }
+        catch (TimeoutException e) {
+        	context.getLogger().log("Edge Device timed out: " + e.getMessage());
+            output = buildEndResponseBody("The edge device did not respond in time.");
         }
         catch (Exception e) {
-        	context.getLogger().log("EXCEPTION: " + e.getMessage() + "\n");
+        	context.getLogger().log("EXCEPTION: " + e.getMessage());
+            output = buildEndResponseBody("Something went wrong.");
         }
  
-		context.getLogger().log("Output Map: " + map + "\n");
+        if(output == null) {
+        	output = buildResponse("This is not expected. Please try again.", true /* end session */);
+        }
+		context.getLogger().log("Output Map: " + output);
 
-        return map;
+        return output;
     }
     
     static Map<String,Object> getMap(Map<String,Object> map, String[] keys) {
@@ -74,28 +101,158 @@ public class LambdaFunctionHandler implements RequestHandler<Map<String,Object>,
     	}
     }
     
-    private JSONObject buildResponseBody(String text, boolean endSession) throws Exception {
-        JSONObject responseBody = new JSONObject();
+    private Map<String, Object> buildEndResponseBody(String text) {
+        return buildResponseBody(text, true /* end Session */, null /* no session state */);
+    }
+    
+    private Map<String, Object> buildResponseBody(String text, boolean endSession, Map<String, Object> sessionAttributes) {
+    	Map<String, Object> responseBody = new HashMap<String, Object>();
         responseBody.put("version", "1.0");
-        responseBody.put("sessionAttributes", new JSONObject());
+        if(!endSession && (sessionAttributes != null)) {
+        	responseBody.put("sessionAttributes", sessionAttributes);
+        }
         responseBody.put("response", buildResponse(text, endSession));
 
         return responseBody;
     }
 
-    private JSONObject buildResponse(String text, boolean endSession) throws Exception {
-        JSONObject response = new JSONObject();
+    private Map<String, Object> buildResponse(String text, boolean endSession) {
+    	Map<String, Object> response = new HashMap<String, Object>();
         response.put("outputSpeech", buildOutputSpeech(text));
         response.put("shouldEndSession", endSession);
 
         return response;
     }
 
-    private JSONObject buildOutputSpeech(String message) throws Exception {
-    	JSONObject text = new JSONObject();
+    private Map<String, Object> buildOutputSpeech(String message) {
+    	Map<String, Object> text = new HashMap<>();
     	text.put("type", "PlainText");
     	text.put("text", message);
     	
     	return(text);
     }
+    
+    private Future<JSONObject> sendEdgeRequest(String imageId, String tag, Context context) throws Exception {
+    	String awsAccessKeyId = System.getenv("awsAccessKeyId");
+    	String awsSecretAccessKey = System.getenv("awsSecretAccessKey");
+    	String endpoint = System.getProperty("awsIoTEndpoint", "a131ws0b6gtght.iot.us-west-2.amazonaws.com");
+
+    	if ((awsAccessKeyId == null) || (awsSecretAccessKey == null) || awsAccessKeyId.isEmpty() || awsSecretAccessKey.isEmpty()) {
+    		throw new Exception("No edge device credentials.");
+    	}
+    	
+    	AWSIotMqttClient client = new AWSIotMqttClient(endpoint, EDGE_DEVICE_CLIENT_ID, awsAccessKeyId, awsSecretAccessKey);    	
+		client.connect();
+	
+		context.getLogger().log("Connected to IoT queue\n");
+	
+		TopicResponseListener listener = new TopicResponseListener(client, TOPIC, RESPONSE_TYPE_CAMERA, RESPONSE_REPLY_IMAGE, context);
+   
+		JSONObject arguments = new JSONObject();
+		arguments.put("mode", "image");
+		JSONObject command = new JSONObject();
+		command.put("type", "camera");
+		command.put("action", "capture");
+		command.put("arguments", arguments);
+		client.publish(TOPIC, AWSIotQos.QOS0, command.toString());
+		
+		return listener;
+	}
+    
+    private static class TopicResponseListener extends AWSIotTopic implements Future<JSONObject> {
+    	
+    	private AWSIotMqttClient client;
+    	private String type;
+    	private String reply;
+    	private Context context;
+    	private JSONObject result = null;
+    	private Exception exception = null;
+    	
+        public TopicResponseListener(AWSIotMqttClient client, String topic, String type, String reply, Context context) 
+        	throws Exception {
+            super(topic, AWSIotQos.QOS0);
+            this.client = client;
+            this.type = type;
+            this.reply = reply;
+            this.context = context;
+            
+            this.client.subscribe(this);
+        }
+
+        @Override
+        public void onMessage(AWSIotMessage message) {
+            context.getLogger().log("IoT Queue: " + message.getTopic() + " => " + message.getStringPayload());
+        	if(!isDone()) {
+        		try {
+		            JSONObject payload = new JSONObject(message.getStringPayload());
+		            if(type.equals(payload.optString("type")) && reply.equals(payload.optString("reply"))) {
+		            	synchronized (this) {
+		            		result = payload;
+		            	}
+		            	client.unsubscribe(this);
+		            	client.disconnect(100, true /* blocking */);
+		            }
+        		}
+        		catch (Exception e) {
+	            	synchronized (this) {
+	            		exception = e;
+	            	}
+        		}
+        	}
+        }
+
+		@Override
+		public boolean cancel(boolean mayInterruptIfRunning) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public boolean isCancelled() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public synchronized boolean isDone() {
+			return (result != null) || (exception != null);
+		}
+
+		@Override
+		public JSONObject get() throws InterruptedException, ExecutionException {
+			
+			while(!isDone()) {
+				Thread.sleep(100);
+			}
+			
+			if(exception != null) {
+				throw new ExecutionException(exception);
+			}
+
+			return result;
+		}
+
+		@Override
+		public JSONObject get(long timeout, TimeUnit unit)
+				throws InterruptedException, ExecutionException, TimeoutException {
+			
+			if(!isDone()) {
+				long millisTimeout = TimeUnit.MILLISECONDS.convert(timeout, unit);
+				Thread.sleep(millisTimeout);
+			}
+			
+			if(!isDone()) {
+				throw new TimeoutException();
+			}
+
+			if(exception != null) {
+				throw new ExecutionException(exception);
+			}
+
+			return result;
+		}
+        
+        
+    }
+
 }
